6a82a6c5bc8ff7b1672cc6aaff0cb33245728a58
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-03 14:58:39 +0530

    routes added

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/login', function (req, res) {
+    res.render('login');
+});
+
+app.get('/register', function (req, res) {
+    res.render('register');
+});
+
 app.listen(process.env.PORT || 3000, function () {
     console.log('Server is started');
 });
