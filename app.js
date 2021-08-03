5229571892cbb86c778bb40521d4ec6fcd574447
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-03 14:27:20 +0530

    starter files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Server is started');
+});
