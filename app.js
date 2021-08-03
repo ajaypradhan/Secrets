427477e42ede284c0f9a4ed6427bb4e477dbe6bd
Ajay Pradhan <pradhanajay14@gmail.com>
2021-08-03 15:27:15 +0530

    register user

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const userSchema = {
+    email: String,
+    password: String,
+};
+const User = new mongoose.model('User', userSchema);
 
 app.get('/', function (req, res) {
     res.render('home');
@@ -19,6 +31,23 @@ app.get('/login', function (req, res) {
 app.get('/register', function (req, res) {
     res.render('register');
 });
+
+//register a new user 
+app.post('/register', function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+});
+
+
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Server is started');
